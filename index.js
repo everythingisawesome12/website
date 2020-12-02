7098f41d41e7feb9044cbffee003d156577cbd06
everythingisawesome12 <42191358+everythingisawesome12@users.noreply.github.com>
2020-12-01 21:46:39 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+var randomnumber1 = Math.floor(Math.random() * 6) + 1;
+var image = "images/dice" + randomnumber1 + ".png";
+document.querySelectorAll("img")[0].setAttribute("src", image);
+var randomnumber2 = Math.floor(Math.random() * 6) + 1;
+var image2 = "images/dice" + randomnumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src", image2);
